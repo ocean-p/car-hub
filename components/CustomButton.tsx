b21d1bf669f51a3afef7ec5f3d4dc2a583e6fbbe
ocean-p <duongkodoiqua@gmail.com>
2023-07-18 13:59:29 +0700

    handle render list and search

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,7 +3,8 @@ import { CustomButtonProps } from "@/types"
 import Image from "next/image"
 
 const CustomButton = (props : CustomButtonProps) => {
-  const {title, containerStyles, handleClick, btnType} = props
+  const {title, containerStyles, handleClick, btnType,
+    textStyles, rightIcon, isDisabled} = props
 
   return (
     <button
@@ -12,9 +13,19 @@ const CustomButton = (props : CustomButtonProps) => {
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className="flex-1">
+      <span className={`flex-1 ${textStyles}`}>
         {title}
       </span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   )
 }
